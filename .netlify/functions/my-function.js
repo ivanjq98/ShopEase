938a6abcafb7aabb91718ec44a7e3496167bd58c
ivanjq98 <tan.ivancjq@gmail.com>
2025-01-29 02:22:52 +0800

    netlify toml and my-function

--- .netlify/functions/my-function.js
+++ .netlify/functions/my-function.js
@@ -1,7 +1,8 @@
+// netlify/functions/my-function.js
 exports.handler = async function(event, context) {
           return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Hello from backend!" }),
+            body: JSON.stringify({ message: 'Hello from the serverless function!' })
           };
         };
         
