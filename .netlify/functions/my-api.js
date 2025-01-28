d7402fd944cdaf7e79de19e78087f62b43e9d906
ivanjq98 <tan.ivancjq@gmail.com>
2025-01-29 02:04:27 +0800

    build remove 2 variables

--- .netlify/functions/my-api.js
+++
@@ -1,8 +0,0 @@
-// ./netlify/functions/my-api.js
-exports.handler = async function(event, context) {
-          return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Hello from backend API!' })
-          }
-        }
-        
