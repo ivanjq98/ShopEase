b4dea623b34f17cc23cfbd2793c6edcf082287d0
ivanjq98 <tan.ivancjq@gmail.com>
2025-01-29 00:18:42 +0800

    deployment

---
+++ .netlify/functions/my-api.js
@@ -0,0 +1,8 @@
+// ./netlify/functions/my-api.js
+exports.handler = async function(event, context) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello from backend API!' })
+          }
+        }
+        
