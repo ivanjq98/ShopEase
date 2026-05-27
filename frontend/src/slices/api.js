b55ef9b3ad6e354cdb746a8b38f10abdec58f1de
Ivan Tan <tan.ivancjq@gmail.com>
2026-05-27 19:59:28 +0800

    [Backend] Url

--- frontend/src/slices/api.js
+++ frontend/src/slices/api.js
@@ -1,4 +1,4 @@
-export const url = "https://shopease-1-5tkn.onrender.com"
+export const url = "https://shopease-1-5tkn.onrender.com/api"
 
 export const setHeaders = () => {
   const token = localStorage.getItem("token");
